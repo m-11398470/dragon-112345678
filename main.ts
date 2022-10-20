d1977ec0bf1eac76171f50c0afc79367049d3001
m-11398470 <115676827+m-11398470@users.noreply.github.com>
2022-10-20 10:09:36 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,23 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() == 80) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
+})
