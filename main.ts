888395d8832c3d3d6ab0310094f9ba30515cec3e
m-11398470 <115676827+m-11398470@users.noreply.github.com>
2022-10-27 09:57:39 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,9 @@ basic.forever(function () {
         basic.pause(200)
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(200)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        basic.pause(200)
         pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(200)
     }
 })
